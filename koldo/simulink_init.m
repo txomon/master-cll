ee301141dcef1a03c7df7a823babb8cbbec36284
Javier Domingo Cansino <javierdo1@gmail.com>
2015-05-18 17:43:00 +0200

    Adding koldo's exercises and initial config Paths should be for library a folder

---
+++ koldo/simulink_init.m
@@ -0,0 +1,15 @@
+C = tf([ 0.525 5.022 4.4 ], [ 0.005 1 0]);
+G = tf([ 52.1 ] , [ 1.21 1 0 ]);
+C.InputName = 'e';
+C.OutputName = 'u';
+G.InputName = 'u';
+G.OutputName = 'y';
+Sum = sumblk('e', 'r', 'y' , '+-');
+controlled = connect(G, C, Sum, 'r', 'y');
+Ts = 1/bandwidth(controlled);
+Ts1 = Ts/15;
+Ts2 = Ts/150;
+
+% Names in the simulink model...
+T2 = Ts1;
+CT2_tust = c2d(C, Ts1, 'tustin');
